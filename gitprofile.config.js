2bae517642767350e90bf8045f2b62ce7acc8304
Muhammad Sholeh <60766389+Assyatier21@users.noreply.github.com>
2023-01-15 15:42:29 +0700

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -4,7 +4,7 @@ const config = {
   github: {
     username: 'Assyatier21', // Your GitHub org/user name. (Required)
     sortBy: 'stars', // stars | updated
-    limit: 10, // How many projects to display.
+    limit: 4, // How many projects to display.
     exclude: {
       forks: false, // Forked projects will not be displayed if set to true.
       projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
@@ -26,38 +26,58 @@ const config = {
   },
   resume: {
     fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      '', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
+    'Go',
+    'Java',
     'PHP',
-    'Laravel',
-    'JavaScript',
-    'React.js',
     'Node.js',
-    'Nest.js',
+    'Laravel',
+    'Echo-Framework',
+    'Bootstrap',
+    'React.js',
+    'Next.js',
     'MySQL',
     'PostgreSQL',
+    'GraphQL',
+    'Elasticsearch',
+    'Jenkins',
     'Git',
+    'GitHub',
+    'GitLab',
     'Docker',
-    'PHPUnit',
-    'CSS',
-    'Antd',
-    'Tailwind',
+    'Unit Testing',
+    'Sonarqube',
   ],
   experiences: [
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'September 2021',
-      to: 'Present',
-      companyLink: 'https://example.com',
+      company: 'Tokopedia',
+      position: 'Software Engineer (Backend Developer at Seller Education)',
+      from: 'September 2022',
+      to: 'December 2022',
+      companyLink: 'https://seller.tokopedia.com/edu',
     },
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'July 2019',
-      to: 'August 2021',
-      companyLink: 'https://example.com',
+      company: 'Kisel Indonesia',
+      position: 'Website Developer',
+      from: 'January 2022',
+      to: 'September 2022',
+      companyLink: 'https://kiselindonesia.com/',
+    },
+    {
+      company: 'RSUD Siti Fatimah Prov. Sumsel',
+      position: 'Frontend Developer Internship',
+      from: 'September 2021',
+      to: 'December 2022',
+      companyLink: 'https://sifalib.sifa.co.id/',
+    },
+    {
+      company: 'Huawei Tech Investment',
+      position: 'Backend Developer Internship',
+      from: 'September 2021',
+      to: 'December 2022',
+      companyLink: 'https://www.huawei.com/en/',
     },
   ],
   /* certifications: [
@@ -70,41 +90,42 @@ const config = {
   ], */
   education: [
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2015',
-      to: '2019',
-    },
-    {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2012',
-      to: '2014',
+      institution: 'Sriwijaya University',
+      degree: 'Computer Science',
+      from: '2018',
+      to: '2022',
     },
   ],
 
   // To hide the `My Projects` section, keep it empty.
   externalProjects: [
     {
-      title: 'Project Name',
+      title: 'Aplikasi Lelang Aset (ALEA)',
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-      imageUrl: 'https://via.placeholder.com/250x250',
-      link: 'https://example.com',
+        'An application for asset auctions belonging to the Koperasi Telekomunikasi Seluler (Kisel) which is spread across various regions in Indonesia. This application is intended for Telkomsel and Kisel employees and general users outside of the company.',
+      imageUrl: '',
+      link: 'http://alea.kiselindonesia.com:8083/',
     },
     {
-      title: 'Project Name',
+      title: 'Siti Fatimah Digital Library (SIFALIB)',
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-      imageUrl: 'https://via.placeholder.com/250x250',
-      link: 'https://example.com',
+        'An application that aims to be a Digital Library belonging to the RSUD Siti Fatimah Provinsi Sumatera Selatan. This digital library will be a place for digital documents regarding research that has been carried out in the hospital environment as well as research conducted by researchers from the hospital.',
+      imageUrl: '',
+      link: 'https://sifalib.sifa.co.id/',
+    },
+    {
+      title: 'Sistem Informasi Pendidikan Berkelanjutan (SI-DILAN)',
+      description:
+        'An application is used to apply for study permits or study assignments for civil servants in hospitals. This application also provides institutional information for employees who want to continue their studies as well as scholarship information in it. This application is connected through a centralized gateway called sifa.co.id.',
+      imageUrl: '',
+      link: 'https://dilan.sifa.co.id/',
     },
   ],
   // Display blog posts from your medium or dev account. (Optional)
   blog: {
-    source: 'dev', // medium | dev
-    username: 'arifszn', // to hide blog section, keep it empty
-    limit: 2, // How many posts to display. Max is 10.
+    source: '', // medium | dev
+    username: '', // to hide blog section, keep it empty
+    limit: , // How many posts to display. Max is 10.
   },
   googleAnalytics: {
     id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
@@ -115,7 +136,7 @@ const config = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'winter',
+    defaultTheme: 'dark',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
